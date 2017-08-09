d6149ce458ebd302b9da0a3c818b8d6399221506
utahraul <raul.hernandez@testeandosoftware.com>
2017-08-09 11:42:48 +0200

    Proyecto de sensor de temperatura y humedad

---
+++ tesperaturaHumedadThingSpeak.ino/tesperaturaHumedadThingSpeak.ino.ino
@@ -0,0 +1,91 @@
+#include <DHT.h>
+#include <ESP8266WiFi.h>
+ 
+// replace with your channel’s thingspeak API key and your SSID and password
+String apiKey = "thingspeak api key";
+const char* ssid = "SSID WiFi";
+const char* password = "Password WiFi";
+const char* server = "api.thingspeak.com";
+
+//DHTPIN es el pin la tarjeta donde conectamos el sensor 
+#define DHTPIN D5
+#define DHTTYPE DHT11 
+ 
+DHT dht(DHTPIN, DHTTYPE);
+WiFiClient client;
+ 
+void setup() 
+{
+Serial.begin(115200);
+delay(10);
+dht.begin();
+ 
+WiFi.begin(ssid, password);
+ 
+Serial.println();
+Serial.println();
+Serial.print("Conectando a ");
+Serial.println(ssid);
+ 
+WiFi.begin(ssid, password);
+ 
+while (WiFi.status() != WL_CONNECTED) 
+{
+delay(500);
+Serial.print(".");
+}
+Serial.println("");
+Serial.println("WiFi connected");
+ 
+}
+ 
+void loop() 
+{
+ 
+float h = dht.readHumidity();
+float t = dht.readTemperature();
+if (isnan(h) || isnan(t)) 
+{
+Serial.println("Failed to read from DHT sensor!");
+return;
+}
+ 
+if (client.connect(server,80)) {
+String postStr = apiKey;
+postStr +="&field1=";
+postStr += String(t);
+postStr +="&field2=";
+postStr += String(h);
+postStr += "\r\n\r\n";
+ 
+client.print("POST /update HTTP/1.1\n");
+client.print("Host: api.thingspeak.com\n");
+client.print("Connection: close\n");
+client.print("X-THINGSPEAKAPIKEY: "+apiKey+"\n");
+client.print("Content-Type: application/x-www-form-urlencoded\n");
+client.print("Content-Length: ");
+client.print(postStr.length());
+client.print("\n\n");
+client.print(postStr);
+ 
+Serial.print("Temperature: ");
+Serial.print(t);
+Serial.print(" degrees Celsius Humidity: ");
+Serial.print(h);
+Serial.println(" . Sending data to Thingspeak");
+  
+  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);              // wait for a second
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);              // wait for a second
+  
+}
+client.stop();
+ 
+Serial.println("Waiting 5 mins");
+// thingspeak needs at least a 15 sec delay between updates
+// 5 minutes to be safe
+delay(300000);
+}
+
+
